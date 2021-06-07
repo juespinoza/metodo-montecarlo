5523d26e8b38d08a2d7e03848ab10669b4b72944
Julia Espinoza <juespinozacorrea@gmail.com>
2021-06-07 01:33:52 -0300

    correccion de codigo

--- calculos.js
+++ calculos.js
@@ -1,81 +1,53 @@
 window.onload = function () {
-  //Definimos una función para generar los puntos aleatorios que se dispararán (teniendo en cuenta los límites del rectangulo)
+  // Función que calcula la integral aproximada
+  // Fórmula: (disparosEnArea / disparosTotales) * areaDelRectangulo
+  function getIntegralAproximadaMontecarlo(
+    disparosEnArea,
+    disparosTotales,
+    areaRectangulo
+  ) {
+    return (
+      (parseFloat(disparosEnArea) / disparosTotales) *
+      parseFloat(areaRectangulo)
+    );
+  }
+
+  // Función para generar puntos aleatorios en un rango de minimo y máximo.
+  // Resultado: puntos disparados en eje
   function generarPuntoAleatorio(min, max) {
     random = parseFloat(Math.random() * (max - min) + min).toFixed(4);
 
     return random;
   }
 
-  //Definimos el Algoritmo de Newton-Rapson
-  function newtonraphson(funcion, funcionDerivada, valorInicial) {
-    var tolerancia = 1e-7;
-    var epsilon = 2.220446049250313e-16;
-    var maxCount = 20;
-    var count = 0;
-    var raiz;
-
-    while (count++ < maxCount) {
-      imagenFuncionEnValorInicial = math
-        .evaluate(funcion, { x: valorInicial })
-        .toString();
-      imagenFuncionDerivadaEnValorInicial = math
-        .evaluate(funcionDerivada, { x: valorInicial })
-        .toString();
-
-      if (
-        Math.abs(imagenFuncionDerivadaEnValorInicial) <=
-        epsilon * Math.abs(imagenFuncionEnValorInicial)
-      ) {
-        console.log("Not Convergent");
-        return false;
-      }
-
-      raiz =
-        valorInicial -
-        imagenFuncionEnValorInicial / imagenFuncionDerivadaEnValorInicial;
-
-      if (Math.abs(raiz - valorInicial) <= tolerancia * Math.abs(raiz)) {
-        return Math.round(raiz * 100.0) / 100.0;
-      }
-
-      valorInicial = raiz;
-    }
-    return false;
-  }
-
-  //Cálculamos Máx y Mín
-  function calcularMaxMin(a, b, funcion) {
-    //Definimos el valor N* de la ecuación Xk = a + (k(b-a))/N*
+  // Función para calcular los puntos máximo y mínimo de la función
+  // Fórmula: Xk = a + (k(b-a))/N*
+  function calcularPuntosMaxMin(a, b, funcion) {
     let n = (b - a) * 10;
-    //hallamos las Xk y las guardamos en un vector
 
-    let vector = new Array(n).fill(undefined).map((value, k) => {
+    // Generamos valores para Xk y los guardamos en un vector
+    let vectorXk = new Array(n).fill(undefined).map((value, k) => {
       return parseFloat(a) + k * ((parseFloat(b) - parseFloat(a)) / n);
     });
-    //Con los valores XK, calculamos los valores de F(xk)
-    let valores = vector.map((valor) => {
+
+    // Calculamos los valores f(Xk) y los guardamos en un vector
+    let valores = vectorXk.map((valor) => {
       return math.evaluate(funcion, { x: valor }).toString();
     });
 
-    //Definimos los valores Max y Min del rectángulo
+    // Definimos los valores MAX y MIN
     let valormax = Math.max.apply(Math, valores);
     let valormin = Math.min.apply(Math, valores);
 
-    if (valormax >= 0) {
-      valormax = 1.1 * valormax;
-    } else {
-      valormax = 0;
-    }
-    if (valormin >= 0) {
-      valormin = 0;
-    } else {
-      valormin = 1.1 * valormin;
-    }
+    // Aumentamos los rangos un 10% para dibujar un rectangulo un poco aumentado
+    valormax = valormax >= 0 ? 1.1 * valormax : 0;
+    valormin = valormin >= 0 ? 0 : 1.1 * valormin;
 
     return [valormax, valormin];
   }
 
-  function valuesOk() {
+  // Comprobamos que existan las entradas para ejecutar la función montecarlo
+  function valoresOk() {
     return (
       document.getElementById("funcion").value &&
       document.getElementById("a").value &&
@@ -84,92 +56,95 @@ window.onload = function () {
     );
   }
 
-  //Definimos una función para tomar los valores que ingreso el usuario y realizamos el método de Monte Carlo
+  // Función que calcula todos los pasos y construye el grafico de la funcion y los puntos disparados.
   function montecarlo() {
-    if (valuesOk) {
+    if (valoresOk) {
+      // PASO 1: se leen los datos ingresados por el usuario.
       let y = document.getElementById("funcion").value;
       let a = document.getElementById("a").value;
       let b = document.getElementById("b").value;
-      let disparos = document.getElementById("disparos").value;
+      let disparosTotal = document.getElementById("disparos").value;
+
+      // PASO 2: calcular los puntos máximo y mínimo de la función
       let ymax;
       let ymin;
-      let raices = [];
-      let max = [];
-      let min = [];
-
-      //PASO 2
-      // Definimos el máx y mín del rectangulo
-      let maxMin = calcularMaxMin(a, b, y);
+      let maxMin = calcularPuntosMaxMin(a, b, y);
       ymax = maxMin[0];
       ymin = maxMin[1];
 
-      //PASO 3
-      //Calculamos el área del rectangulo
+      // PASO 3: calcular el área del rectángulo tomando los límites de cada eje (a, b, ymin, ymax)
       let areaRectangulo =
         (parseFloat(b) - parseFloat(a)) * (parseFloat(ymax) - parseFloat(ymin));
 
-      //PASO 4
-      //Definimos los contadores de los disparos totales y los disparos en el área definida
+      // PASO 4: calcular el valor aproximado de la integral definida
+      // Fórmula: (( disparosEnArea / disparosTotal )*(b-a)) * (ymax - ymin)
+
+      // Realizamos los disparos y seteamos los arrays que contienen los puntos adentro y fuera del area
       let disparosTotales = 0;
       let disparosEnArea = 0;
-
-      //Definimos los arrays de puntos aleatorios
-      let puntosInX = [];
-      let puntosInY = [];
-      let puntosOutX = [];
-      let puntosOutY = [];
+      let puntosEnX = [];
+      let puntosEnY = [];
+      let puntosFueraX = [];
+      let puntosFueraY = [];
       let puntosInColor = [];
       let puntosOutColor = [];
 
       //Definimos un ciclo para cada disparo
-      while (disparosTotales < disparos) {
+      while (disparosTotales < disparosTotal) {
         let puntoX = generarPuntoAleatorio(parseFloat(a), parseFloat(b));
         let puntoY = generarPuntoAleatorio(parseFloat(ymin), parseFloat(ymax));
 
-        let inColor = "rgba(9, 107, 5, 0.5)";
-        let outColor = "rgba(145, 48, 35, 0.5)";
+        let enColor = "rgba(9, 107, 5, 0.5)";
+        let fueraColor = "rgba(145, 48, 35, 0.5)";
 
-        if (
-          parseFloat(math.evaluate(y, { x: parseFloat(puntoX) }).toString()) >
-            parseFloat(puntoY) &&
-          parseFloat(puntoY) > 0
-        ) {
+        let valorFuncionPuntoX = parseFloat(
+          math.evaluate(y, { x: parseFloat(puntoX) }).toString()
+        );
+
+        if (valorFuncionPuntoX > parseFloat(puntoY) && parseFloat(puntoY) > 0) {
+          // Si f(x) > y && y > 0 está en el área positiva
           disparosEnArea++;
-          puntosInX.push(puntoX);
-          puntosInY.push(puntoY);
-          puntosInColor.push(inColor);
+
+          // Se cargan los puntos como puntos en area y se carga el color
+          puntosEnX.push(puntoX);
+          puntosEnY.push(puntoY);
+          puntosInColor.push(enColor);
         } else if (
-          parseFloat(math.evaluate(y, { x: parseFloat(puntoX) }).toString()) <
-            parseFloat(puntoY) &&
+          valorFuncionPuntoX < parseFloat(puntoY) &&
           parseFloat(puntoY) < 0
         ) {
+          // Si f(x) > y && y > 0 está en el área negativa
           disparosEnArea--;
-          puntosInX.push(puntoX);
-          puntosInY.push(puntoY);
-          puntosInColor.push(inColor);
+
+          // Se cargan los puntos como puntos en area y se carga el color
+          puntosEnX.push(puntoX);
+          puntosEnY.push(puntoY);
+          puntosInColor.push(enColor);
         } else {
-          puntosOutX.push(puntoX);
-          puntosOutY.push(puntoY);
-          puntosOutColor.push(outColor);
+          // Se cargan los puntos como puntos fuera de area y se carga el color
+          puntosFueraX.push(puntoX);
+          puntosFueraY.push(puntoY);
+          puntosOutColor.push(fueraColor);
         }
 
         disparosTotales++;
       }
 
-      //Realizamos una funcion para obtener el valor de la integral
-      function getMonteCarlo() {
-        let estimacionIntegral =
-          (parseFloat(disparosEnArea) / disparosTotales) *
-          parseFloat(areaRectangulo);
-        return estimacionIntegral;
-      }
+      // Obtenemos el valor de la integral aproximada para mostrarla
+      let integralAproximada = getIntegralAproximadaMontecarlo(
+        disparosEnArea,
+        disparosTotales,
+        areaRectangulo
+      );
+      document.getElementById("area").innerHTML = integralAproximada;
 
+      // Límites aumentados para hacer el gráfico
       let xMinLimit = a > 0 ? a * 0.8 : a * 1.2;
       let xMaxLimit = b > 0 ? b * 1.2 : b * 0.8;
       let yMinLimit = ymin > 0 ? ymin * 0.8 : ymin * 1.2;
       let yMaxLimit = ymax > 0 ? ymax * 1.2 : ymax * 0.8;
 
-      //Realizamos el gráfico de la solución
+      // Creamos los valores del gráfico
       var layout = {
         title: "Gráfico de la función mediante el Método Montecarlo",
         xaxis: {
@@ -193,22 +168,19 @@ window.onload = function () {
               color: "rgba(85, 87, 85, 1)",
               width: 2,
             },
-            //fillcolor: "rgba(255, 99, 71, 0.1)",
           },
         ],
       };
 
-      const exprFuncion = math.compile(y);
-      const xValuesFuncion = math
-        .range(xMinLimit, xMaxLimit, 0.1, true)
-        .toArray();
-      const yValuesFuncion = xValuesFuncion.map(function (x) {
-        return exprFuncion.evaluate({ x: x });
+      const usuarioFuncion = math.compile(y);
+      const valoresX = math.range(xMinLimit, xMaxLimit, 0.1, true).toArray();
+      const valoresY = valoresX.map(function (x) {
+        return usuarioFuncion.evaluate({ x: x });
       });
 
-      var funcion = {
-        x: xValuesFuncion,
-        y: yValuesFuncion,
+      var funcionLinea = {
+        x: valoresX,
+        y: valoresY,
         mode: "lines",
         fill: "tozeroy",
         type: "scatter",
@@ -216,8 +188,8 @@ window.onload = function () {
       };
 
       var puntosEnArea = {
-        x: puntosInX,
-        y: puntosInY,
+        x: puntosEnX,
+        y: puntosEnY,
         mode: "markers",
         type: "scatter",
         name: "Puntos en área",
@@ -228,8 +200,8 @@ window.onload = function () {
       };
 
       var puntosFueraArea = {
-        x: puntosOutX,
-        y: puntosOutY,
+        x: puntosFueraX,
+        y: puntosFueraY,
         mode: "markers",
         type: "scatter",
         name: "Puntos fuera área",
@@ -239,19 +211,18 @@ window.onload = function () {
         },
       };
 
-      var data = [funcion, puntosEnArea, puntosFueraArea];
+      var data = [funcionLinea, puntosEnArea, puntosFueraArea];
 
       Plotly.newPlot("myDiv", data, layout);
-      document.getElementById("area").innerHTML = getMonteCarlo();
     }
   }
 
-  //Actualizamos el gráfico cada vez que el usuario presiona el botón "Calcular"
+  // Refrescar los valores del resultado cuando el usuario vuelve a calcular
   document.getElementById("form").onsubmit = function (event) {
     event.preventDefault();
     montecarlo();
   };
 
-  //Realizamos el Método de Montecarlo
+  // Se calculan los valores del resultado al iniciar la aplicación
   montecarlo();
 };
